2f1eb6605d339d463bde6e9135267f702f3a718a
Danihcv <dhcv@ic.ufal.br>
2018-12-18 23:28:11 -0300

    adiciona lógica de turnos e de inserção de gaps

--- genetic algorithm/alinhamento multiplo.py
+++ genetic algorithm/alinhamento multiplo.py
@@ -1,12 +1,8 @@
+import sys
+
 alfa = int(input())
 beta = int(input())
 gama = int(input())
-
-
-class DNA:
-    def __init__(self, sequence):
-        self.current_gap = -1
-        self.sequence = sequence
 
 
 def value_of_pair(seq1, seq2):
@@ -35,17 +31,59 @@ def value_of_state(DNAs):
     return total
 
 
+def generate_turns(i, a, total=[]):
+    if i == 0:
+        total += [a]
+        return
+    for j in range(2):
+        generate_turns(i - 1, a + [j], total)
+
+    return total
+
+
+def standardize_sequences(sequences_list, to_size):
+    for i in range(len(sequences_list)):
+        sequences_list[i] = sequences_list[i] + ('-' * (to_size - len(sequences_list[i])))
+
+
 dna_list = []
-biggest_sequence = 0
+standard_size = 0
 while True:
     try:
         seq = input()
-        dna_list += [DNA(seq)]
+        dna_list += [seq]
 
-        if len(seq) > biggest_sequence:
-            biggest_sequence = len(seq)
+        if len(seq) > standard_size:
+            standard_size = len(seq)
     except:
         break
 
-print(dna_list)
+turns = generate_turns(len(dna_list), [])
+pos = 0
+best_score = -sys.maxsize
+while True:
+    if pos >= standard_size:
+        break
+
+    print('- dna_list:', dna_list)
+    print('- standard_size:', standard_size)
+    print('- current position:', str(pos) + '/' + str(standard_size - 1))
+
+    for turn in turns:
+        dna_list_copy = dna_list.copy()
+        local_standard_size = standard_size
+        print('turn:', turn)
+        for seq, insert_gap in enumerate(turn):
+            print('\tseq:', seq, ', insert_gap:', insert_gap)
+
+            if insert_gap == 1 and pos < len(dna_list_copy[seq]):
+                dna_list_copy[seq] = dna_list_copy[seq][:pos] + '-' + dna_list_copy[seq][pos:]
+
+                if len(dna_list_copy[seq] > local_standard_size):
+                    local_standard_size = len(dna_list_copy[seq])
+
+        print(dna_list_copy)  # evaluation point
+    print()
+
+    pos += 1
 
